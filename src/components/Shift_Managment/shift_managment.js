ba9b9213117131a65eb7f38f46204500acd2b4e0
unknown <daniyalafzal0008@gmail.com>
2024-09-07 16:44:02 +0500

    updated

--- src/components/Shift_Managment/shift_managment.js
+++ src/components/Shift_Managment/shift_managment.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
 import "../Settings/Setting_Tabs/leave.css";
@@ -299,76 +298,6 @@ const ShiftsTable = () => {
                     </tbody>
                 </table>
             </div>
-=======
-import ShiftManagementTable from "./ShiftManagementTable";
-
-const Shift_Management = () => {
-    const [data, setData] = useState([
-        {
-            ShiftId: 1,
-            shiftName: "Morning Shift",
-            startTime: "08:00 AM",
-            endTime: "04:00 PM",
-          },
-          {
-            ShiftId: 2,
-            shiftName: "Afternoon Shift",
-            startTime: "12:00 PM",
-            endTime: "08:00 PM",
-          },
-          {
-            ShiftId: 3,
-            shiftName: "Evening Shift",
-            startTime: "04:00 PM",
-            endTime: "12:00 AM",
-          },
-          {
-            ShiftId: 4,
-            shiftName: "Night Shift",
-            startTime: "10:00 PM",
-            endTime: "06:00 AM",
-          },
-          {
-            ShiftId: 5,
-            shiftName: "Early Morning Shift",
-            startTime: "06:00 AM",
-            endTime: "02:00 PM",
-          },
-          {
-            ShiftId: 6,
-            shiftName: "Split Shift",
-            startTime: "08:00 AM",
-            endTime: "12:00 PM",
-          },
-          {
-            ShiftId: 7,
-            shiftName: "Late Evening Shift",
-            startTime: "06:00 PM",
-            endTime: "02:00 AM",
-          },
-          {
-            ShiftId: 8,
-            shiftName: "Graveyard Shift",
-            startTime: "12:00 AM",
-            endTime: "08:00 AM",
-          },
-          {
-            ShiftId: 9,
-            shiftName: "Rotating Shift",
-            startTime: "08:00 AM",
-            endTime: "04:00 PM",
-          },
-          {
-            ShiftId: 10,
-            shiftName: "Weekend Shift",
-            startTime: "10:00 AM",
-            endTime: "06:00 PM",
-          },
-    ])
-    return (
-        <div>
-            <ShiftManagementTable data={data} setData={setData}/>
->>>>>>> 4914598520def6ba1d3c929b7513f8857ed6740d
         </div>
     );
 };
